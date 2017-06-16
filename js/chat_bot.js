bd1ee8f3e43acf60d981e10602d53d8d26b00cb3
Andres Pardini <unpardos@gmail.com>
2017-06-16 09:13:59 -0300

    auto scroll missing arrow icon only lucas user gif

--- js/chat_bot.js
+++ js/chat_bot.js
@@ -2,6 +2,7 @@
 ChatBotApp.controller('ChatController', ['$scope', '$sce' ,'$http', '$timeout', '$interval', '$q', function ($scope, $sce,$http, $timeout, $interval, $q) {
     var vm = this;
     var today = new Date().valueOf()
+    
     // ask for username first time only
     var username = localStorage.getItem("username");
     if (!username) {
@@ -9,36 +10,43 @@ ChatBotApp.controller('ChatController', ['$scope', '$sce' ,'$http', '$timeout', 
         localStorage.setItem("username", username);
     }
     vm.username = username;
-    vm.getMessages = function(){
+    
+    vm.getMessages = function(update){
         $http.get("https://trim-mode-139918.firebaseio.com/.json?print=pretty").then(function(response){
-
-                vm.listMessage = Object.keys(response.data.messages).map(function(key) {
-                                            return response.data.messages[key]
-                                 })
-
-
+            var data = Object.keys(response.data.messages).map(function(key) {
+                return response.data.messages[key]
+            })
+            if (update) {
+                vm.listMessage = data;
+            }
         })
-
-
     }
-    vm.getMessages()
+
+    vm.getMessages(true);
+
 
     function saveMessage (username, text, img) {
         var data = {
             username: username,
             date: new Date().valueOf()
         }
+
         if (text) { data.text = text }
         if (img) { data.img = img }
         if (navigator.onLine){
             firebase.database().ref('messages').push(data);
-
         }else{
             firebase.database().ref('messages').onDisconnect().set(data)
             vm.listMessage.push(data)
         }
-    };
+        
+        $timeout(function(){
+            //scroll chat
+            var div = document.getElementById("chatbody");
+            div.scrollTop = div.scrollHeight;
+        }, 20);
 
+    };
 
     firebase.database().ref("messages").on("value", function(snap){
         if (navigator.onLine){
@@ -57,43 +65,50 @@ ChatBotApp.controller('ChatController', ['$scope', '$sce' ,'$http', '$timeout', 
             vm.verificateUser(input.value);
             vm.verificationBot(input)
             $http.get("http://localhost:3000/server/"+input.value).then(function(response){
-
                 console.log("nice")
-
-
             })
-
-
             input.value = '';
         }
     }
 
     vm.verificateUser = function(text){
+        return
         if(text.includes("ucas")){
             saveMessage(username, undefined, "https://media.giphy.com/media/3o7bu1YKisFPmroLwk/giphy.gif")
         }
     }
 
     vm.verificationBot = function(input){
-        if(input.value.includes("ucas")){
-            saveMessage("moni", undefined, "https://media.giphy.com/media/12XMGIWtrHBl5e/giphy.gif")
-        }else{
-            saveMessage("moni", undefined, "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
-        }
-        if(input.value.includes("prestamo")){
-            saveMessage("moni", "Cuanto dinero quieres?", undefined)
-        }
+        if (vm.username == "lucas") {
+            if(input.value.includes("ucas")){
+                // lucas
+                $timeout(function(){
+                    saveMessage("moni", undefined, "https://media.giphy.com/media/12XMGIWtrHBl5e/giphy.gif")
+                }, 700);
+            }else{
+                // cat
+                $timeout(function(){                
+                    saveMessage("moni", undefined, "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+                }, 700);   
+            }
 
-        if(input.value.includes("orto")){
-            saveMessage("moni", "sigo aqui!!!", undefined)
+            if(input.value.includes("prestamo")){
+                $timeout(function(){                
+                    saveMessage("moni", "Cuanto dinero quieres?", undefined)
+                }, 1400)
+                
+            } else if( input.value.includes("orto") ){
+                $timeout(function(){
+                    saveMessage("moni", "sigo aqui!!!", undefined)
+                }, 1400)                
+            } else if( input.value.includes("vez") ){
+                $timeout(function(){    
+                    saveMessage("moni", "voy a seguir aqui hasta que te mueras!!!", undefined)
+                }, 1400)            
+            }
         }
-        if(input.value.includes("vez")){
-            saveMessage("moni", "voy a seguir aqui hasta que te mueras!!!", undefined)
-        }
-
-
-
     }
+
     vm.verifivateData = function(input, data){
         vm.verifivateUser(data)
         vm.verificationBot(input)
@@ -103,9 +118,7 @@ ChatBotApp.controller('ChatController', ['$scope', '$sce' ,'$http', '$timeout', 
         if (event.which == 13 && !event.shiftKey) {
             $timeout(function () {
                vm.newMessage(event.target)
-
             },1)
         }
-
     }
 }])
